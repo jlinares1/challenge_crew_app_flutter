8fb0d6fc9adcfac35aef6e4de38dfab25d40f96d
jlinares1 <juanjlinares1@gmail.com>
2024-07-06 15:41:18 -0400

    reverting routing back to original

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:challenge_crew_app_flutter/screens/home_screen.dart';
+import 'package:challenge_crew_app_flutter/screens/login_screen.dart';
+import 'package:challenge_crew_app_flutter/screens/profile_creation_screen.dart';
+import 'package:challenge_crew_app_flutter/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:challenge_crew_app_flutter/screens/app_navigation.dart';
+//import 'package:challenge_crew_app_flutter/screens/app_navigation.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,27 +15,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       theme: ThemeData(
           textTheme: GoogleFonts.montserratTextTheme(
         Theme.of(context).textTheme,
       )),
-      routerConfig: AppNavigation.router,
+      initialRoute: LoginScreen.id,
+      routes: {
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        HomeScreen.id: (context) => const HomeScreen(),
+        ProfileCreationScreen.id: (context) => const ProfileCreationScreen()
+      },
     );
   }
 }
 
 
-// MaterialApp(
+
+
+// MaterialApp.router(
 //       theme: ThemeData(
 //           textTheme: GoogleFonts.montserratTextTheme(
 //         Theme.of(context).textTheme,
 //       )),
-//       initialRoute: LoginScreen.id,
-//       routes: {
-//         LoginScreen.id: (context) => const LoginScreen(),
-//         RegistrationScreen.id: (context) => const RegistrationScreen(),
-//         HomeScreen.id: (context) => const HomeScreen(),
-//         ProfileCreationScreen.id: (context) => const ProfileCreationScreen()
-//       },
+//       routerConfig: AppNavigation.router,
 //     );
